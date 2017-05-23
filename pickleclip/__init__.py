9b404456af7ae7edc00743964e1b7d444afdbdd4
Victor Ferraz <victorfsf.dev@gmail.com>
2017-05-23 14:11:08 -0300

    [master] Adding file dump/load from clipboard

--- pickleclip/__init__.py
+++ pickleclip/__init__.py
@@ -4,6 +4,11 @@ from .base import copy
 from .base import dillcopy
 from .base import dillpaste
 from .base import paste
+from .files import load
+from .files import dump
 
 
-__all__ = ['copy', 'paste', 'dillcopy', 'dillpaste']
+__all__ = [
+    'copy', 'paste', 'dillcopy',
+    'dillpaste', 'load', 'dump'
+]
---
+++ pickleclip/files.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from pickleclip.base import paste
+import pyperclip
+
+
+def dump(path, mode='wb'):
+    with open(path, mode) as f:
+        f.write(
+            pyperclip.paste().encode('latin1')
+        )
+    return path
+
+
+def load(path, mode='rb', fn=paste):
+    with open(path, mode) as f:
+        pyperclip.copy(
+            f.read().decode('latin1')
+        )
+    return fn()
